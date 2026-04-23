fd8a22b2253f3c7d4a806bf4ad89200e48c1cdba
mayakaur389 <mayakaur389@gmail.com>
2026-04-23 07:08:35 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,17 +1,5 @@
-import { state, saveState } from './state.js';
 import { updateStats, renderDays } from './ui.js';
-
-function startQuiz(day) {
-  alert(`Day ${day} ka quiz khulega 🔥`);
-  if(!state.done.includes(day)) {
-    state.done.push(day);
-    state.xp += 10;
-    if(state.done.length > state.streak) state.streak = state.done.length;
-    saveState();
-    renderDays(startQuiz);
-    updateStats();
-  }
-}
+import { startQuiz } from './quiz.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   updateStats();
