690dad0ff1f623dfb0f6201f0ebb8fc288d6adb7
mayakaur389 <mayakaur389@gmail.com>
2026-04-25 12:59:01 +0530

    Update lessons.js

--- lessons.js
+++ lessons.js
@@ -1,14 +1,83 @@
 const lessons = [
     { 
         title: "Present Tense", 
+        day: 1,
         questions: [
-            { q: "I ___ to school daily. (go/goes)", answer: "go" }
+            { q: "I ___ to school daily. (go/goes)", answer: "go" },
+            { q: "She ___ a book. (read/reads)", answer: "reads" },
+            { q: "They ___ football. (play/plays)", answer: "play" }
         ]
     },
     { 
         title: "Past Tense", 
+        day: 2,
         questions: [
-            { q: "I ___ to school yesterday. (go/went)", answer: "went" }
+            { q: "I ___ to school yesterday. (go/went)", answer: "went" },
+            { q: "He ___ a movie last night. (watch/watched)", answer: "watched" }
+        ]
+    },
+    { 
+        title: "Future Tense", 
+        day: 3,
+        questions: [
+            { q: "I ___ go to Delhi tomorrow. (will/shall)", answer: "will" },
+            { q: "She ___ come next week. (will/shall)", answer: "will" }
+        ]
+    },
+    { 
+        title: "Nouns & Pronouns", 
+        day: 4,
+        questions: [
+            { q: "___ is my friend. (He/Him)", answer: "He" },
+            { q: "This is ___ book. (my/me)", answer: "my" }
+        ]
+    },
+    { 
+        title: "Verbs", 
+        day: 5,
+        questions: [
+            { q: "I ___ eating. (am/is)", answer: "am" },
+            { q: "They ___ playing. (are/is)", answer: "are" }
+        ]
+    },
+    { 
+        title: "Adjectives", 
+        day: 6,
+        questions: [
+            { q: "She is a ___ girl. (beautiful/beauty)", answer: "beautiful" },
+            { q: "This is a ___ car. (fast/fastly)", answer: "fast" }
+        ]
+    },
+    { 
+        title: "Prepositions", 
+        day: 7,
+        questions: [
+            { q: "She is ___ the park. (in/at)", answer: "in" },
+            { q: "The book is ___ the table. (on/in)", answer: "on" }
+        ]
+    },
+    { 
+        title: "Articles", 
+        day: 8,
+        questions: [
+            { q: "___ sun rises in the east. (A/The)", answer: "The" },
+            { q: "I want ___ apple. (a/an)", answer: "an" }
+        ]
+    },
+    { 
+        title: "Conjunctions", 
+        day: 9,
+        questions: [
+            { q: "I like tea ___ coffee. (and/or)", answer: "and" },
+            { q: "He is poor ___ happy. (but/and)", answer: "but" }
+        ]
+    },
+    { 
+        title: "Question Words", 
+        day: 10,
+        questions: [
+            { q: "___ is your name? (What/Where)", answer: "What" },
+            { q: "___ are you going? (Where/When)", answer: "Where" }
         ]
     }
 ];
