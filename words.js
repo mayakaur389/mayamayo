c5484b0dadac592a138c79d8ebb47161123c2869
mayakaur389 <mayakaur389@gmail.com>
2026-07-29 23:15:14 +0530

    Create words.js

---
+++ words.js
@@ -0,0 +1,18 @@
+const langData = {
+  "hindi-english": {
+    "wrong": {
+      "page_title": "Wrong Questions Practice Karo",
+      "heading": "Galat Sawaal Dobara Sikho",
+      "btn_start": "Practice Shuru Karein",
+      "btn_back": "Wapas Jao"
+    }
+  },
+  "japanese-english": {
+    "wrong": {
+      "page_title": "間違った問題を練習する",
+      "heading": "間違った質問をもう一度学ぶ",
+      "btn_start": "練習を始める",
+      "btn_back": "戻る"
+    }
+  }
+};
