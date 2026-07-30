7699bcefaae26cedcef9d650f19933f740ba2a77
mayakaur389 <mayakaur389@gmail.com>
2026-07-30 14:38:53 +0530

    Update words.js

--- words.js
+++ words.js
@@ -1,26 +1,24 @@
 const langData = {
-  "hindi-english": {
-    "wrong": {
-      "page_title": "Wrong Questions Practice Karo",
-      "heading": "Galat Sawaal Dobara Sikho",
-      "btn_start": "Practice Shuru Karein",
-      "btn_back": "Wapas Jao"
+    "hindi-english": {
+        "wrong": {
+            "page_title": "Wrong Questions Practice Karo",
+            "heading": "Galat Sawaal Dobara Sikho",
+            "btn_start": "Practice Shuru Karein",
+            "btn_back": "Wapas Jao",
+            "total": "Total",
+            "questions_text": "galat questions hain",
+            "no_question": "🔇 Koi galat question nahi hai abhi"
+        }
+    },
+    "japanese-english": {
+        "wrong": {
+            "page_title": "間違った問題を練習する",
+            "heading": "間違った質問をもう一度学ぶ",
+            "btn_start": "練習を始める",
+            "btn_back": "戻る",
+            "total": "合計",
+            "questions_text": "間違った質問",
+            "no_question": "間違った質問はありません！"
+        }
     }
-  },
-  "japanese-english": {
-    "wrong": {
-      "page_title": "間違った問題を練習する",
-      "heading": "間違った質問をもう一度学ぶ",
-      "btn_start": "練習を始める",
-      "btn_back": "戻る"
-    }
-  },
- "japanese-english": {
-  "wrong": {
-    "heading": "間違った質問をもう一度学ぶ",
-    "btn_back": "戻る",
-    "total": "合計", 
-    "no_question": "間違った質問はありません！"
-  }
-} 
 };
