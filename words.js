d43a3a247fe818198e33d880d96e8e6db313f665
mayakaur389 <mayakaur389@gmail.com>
2026-08-01 19:05:51 +0530

    Update words.js

--- words.js
+++ words.js
@@ -1,24 +1,30 @@
 const langData = {
-    "hindi-english": {
-        "wrong": {
-            "page_title": "Wrong Questions Practice Karo",
-            "heading": "Galat Sawaal Dobara Sikho",
-            "btn_start": "Practice Shuru Karein",
-            "btn_back": "Wapas Jao",
-            "total": "Total",
-            "questions_text": "galat questions hain",
-            "no_question": "🔇 Koi galat question nahi hai abhi"
-        }
-    },
-    "ja-en": { // <-- yaha change kiya
-        "wrong": {
-            "page_title": "間違った問題を練習する",
-            "heading": "間違った質問をもう一度学ぶ",
-            "btn_start": "練習を始める",
-            "btn_back": "戻る",
-            "total": "合計",
-            "questions_text": "間違った質問",
-            "no_question": "間違った質問はありません！"
-        }
+  "hindi-english": {
+    "wrong": {
+      "page_title": "Wrong Questions Practice Karo",
+      "heading": "Galat Sawaal Dobara Sikho",
+      "btn_start": "Practice Shuru Karein",
+      "btn_back": "Wapas Jao",
+      "total": "Total",
+      "questions_text": "galat questions hain",
+      "no_question": "🔇 Koi galat question nahi hai abhi", // <-- comma add karo
+      "lessonsBtn": "📖 Lessons",
+      "practiceBtn": "🎙️ Bol Ke Practice",
+      "listenBtn": "👂 Suno"
     }
+  },
+  "ja": {
+    "wrong": {
+      "page_title": "間違った問題を練習する",
+      "heading": "間違った質問をもう一度学ぶ",
+      "btn_start": "練習を始める",
+      "btn_back": "戻る",
+      "total": "合計",
+      "questions_text": "間違った質問",
+      "no_question": "間違った質問はありません！", // <-- comma add karo
+      "lessonsBtn": "📖 レッスン",
+      "practiceBtn": "🎙️ 声に出して練習",
+      "listenBtn": "👂 聞く"
+    }
+  }
 };
