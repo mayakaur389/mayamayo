a1376a61fa8703089a94a224f119a4f4d8a98228
mayakaur389 <mayakaur389@gmail.com>
2026-08-17 15:41:25 +0530

    Update runPracticeQuestion to load immediately
    Refactor practice question loading to execute immediately in practice mode.

--- practice-loader.js
+++ practice-loader.js
@@ -1,36 +1,30 @@
-// Ye function normal quiz start karega
-function startQuizList(){
-  window.showCurrentLesson();
-}
-
-// Ye function practice wala question load karega
 function runPracticeQuestion(){
   const practiceQ = JSON.parse(localStorage.getItem('practiceNow'));
   if(!practiceQ) return;
 
   localStorage.removeItem('practiceNow');
 
-  // lessons ko 1 question wala bana do
-  window.lessons = [{
-    day: "Practice",
-    questions: [practiceQ]
-  }];
+  // Wait karo jab tak lessons load na ho jaye
+  const waitForLessons = setInterval(()=>{
+    if(typeof window.lessons!== 'undefined'){
+      clearInterval(waitForLessons);
 
-  window.currentLessonIndex = 0;
-  window.currentQuestionIndex = 0;
-
-  // normal wala function call
-  window.showCurrentLesson();
+      window.lessons = [{
+        day: "Practice",
+        questions: [practiceQ]
+      }];
+      window.currentLessonIndex = 0;
+      window.currentQuestionIndex = 0;
+      window.showCurrentLesson();
+      window.history.replaceState({}, document.title, "index.html");
+    }
+  }, 100);
 }
 
 document.addEventListener('DOMContentLoaded', function(){
   const urlParams = new URLSearchParams(window.location.search);
 
   if(urlParams.get('practice') === '1'){
-    // practice mode hai to 2 sec baad chalao
-    setTimeout(runPracticeQuestion, 2000);
-  } else {
-    // normal mode hai to seedha quiz start
-    setTimeout(startQuizList, 1000);
+    runPracticeQuestion();
   }
 });
