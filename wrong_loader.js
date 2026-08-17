a8f9a8c8b8f0367100b547db3687667efd1e7e29
mayakaur389 <mayakaur389@gmail.com>
2026-08-17 17:11:35 +0530

    Update and rename practice-loader.js to wrong_loader.js

--- wrong_loader.js
+++ wrong_loader.js
@@ -6,19 +6,17 @@ document.addEventListener('DOMContentLoaded', function(){
     if(!practiceQ) return;
     localStorage.removeItem('practiceNow');
 
-    // 1.5 sec wait karo jab tak 365 load na ho jaye
+    // 365 load hone ka wait karo
     setTimeout(()=>{
-      // Day 1 ke questions ke sabse pehle apna question jod do
       if(window.lessons && window.lessons[0]){
-        window.lessons[0].questions.unshift(practiceQ); // unshift = sabse aage jodna
+        window.lessons[0].questions.unshift(practiceQ); // Day 1 me sabse upar jod do
 
         window.currentLessonIndex = 0;
-        window.currentQuestionIndex = 0; // 0 pe wahi question hoga
-
+        window.currentQuestionIndex = 0;
         window.showCurrentLesson();
         window.history.replaceState({}, document.title, "index.html");
-        console.log("Practice question Day 1 me inject ho gaya");
+        console.log("Wrong question Day 1 me inject ho gaya");
       }
-    }, 1500); // 1.5 second
+    }, 1500);
   }
 });
